34e40b6119d32f375a0fbab47003f3725f769d5d
suhailtajshaik <suhailtajshaik@gmail.com>
2017-09-20 16:25:55 -0400

    YouTube Search API key and npm module added

---
+++ src/config.js
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyBb9uhINHCemfL1hnL69qhogZ08f6DLLxk';
+
+export default API_KEY;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import API_KEY from './config';
 
 const App = () => {
         return (

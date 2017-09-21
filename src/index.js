f37e96e210cb18a629a0d929e3c9c21bcbcb5412
suhailtajshaik <suhailtajshaik@gmail.com>
2017-09-21 16:03:03 -0400

    Video List component init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import API_KEY from './config';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
-        return (
-                <div>
-                        <SearchBar/>
-                </div>
-        );
+class App extends Component {
+        constructor(props) {
+                super(props);
+
+                this.state = {
+                        videos: []
+                }
+                YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => { this.setState({ videos }) })
+        }
+        render() {
+                return (
+                        <div>
+                                <SearchBar />
+                                <VideoList videos={this.state.videos} />
+                        </div>
+                );
+        }
 }
 
-ReactDOM.render(
-        <App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
